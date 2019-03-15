e6635613e622ba7ed0b108d86aab2cc0a97afdca
ssuleymanaylak <ssuleymanaylak@hotmail.com>
2019-03-15 14:21:53 +0300

    Completed OAuth implementation

--- src/App.js
+++ src/App.js
@@ -5,29 +5,54 @@ import Profile from "./Profile";
 import Nav from "./Nav";
 import Auth from "./Auth/Auth";
 import Callback from "./Callback";
+import Public from "./Public";
+import Private from "./Private";
+import Courses from "./Courses";
+import PrivateRoute from "./PrivateRoute";
+import AuthContext from "./AuthContext";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.auth = new Auth(this.props.history);
+    this.state = {
+      auth: new Auth(this.props.history),
+      tokenRenewalComplete: false
+    };
   }
+
+  componentDidMount() {
+    this.state.auth.renewToken(() =>
+      this.setState({ tokenRenewalComplete: true })
+    );
+  }
+
   render() {
+    const { auth } = this.state;
+    //Show loading message until the token rnewal check is completed.
+    if (!this.state.tokenRenewalComplete) return "Loading...";
     return (
-      <>
-        <Nav auth={this.auth} />
+      <AuthContext.Provider value={auth}>
+        <Nav auth={auth} />
         <div className="body">
           <Route
             path="/"
             exact
-            render={props => <Home auth={this.auth} {...props} />}
+            render={props => <Home auth={auth} {...props} />}
           />
           <Route
             path="/callback"
-            render={props => <Callback auth={this.auth} {...props} />}
+            render={props => <Callback auth={auth} {...props} />}
           />
-          <Route path="/profile" component={Profile} />
+          <PrivateRoute path="/profile" component={Profile} />
+          <Route path="/public" component={Public} />
+          <PrivateRoute path="/private" component={Private} />
+          <PrivateRoute
+            path="/courses"
+            component={Courses}
+            scopes={["read:courses"]}
+          />
         </div>
-      </>
+      </AuthContext.Provider>
     );
   }
 }
